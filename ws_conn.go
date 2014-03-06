9665ea8d703a60ea8a6ac8623e9d3b582bef1957
quexer <quexer@gmail.com>
2014-03-06 17:15:25 +0800

    support text frame in web socket

--- ws_conn.go
+++ ws_conn.go
@@ -12,25 +12,42 @@ import (
 
 type wsAdapter struct {
 	conn *websocket.Conn
+	txt  bool
 }
 
 func (p *wsAdapter) Read() ([]byte, error) {
-	var b []byte
-	err := websocket.Message.Receive(p.conn, &b)
-	return b, err
+	if p.txt {
+		var s string
+		err := websocket.Message.Receive(p.conn, &s)
+		return []byte(s), err
+	} else {
+		var b []byte
+		err := websocket.Message.Receive(p.conn, &b)
+		return b, err
+	}
 }
 
 func (p *wsAdapter) Write(b []byte) error {
-	return websocket.Message.Send(p.conn, b)
+	if p.txt {
+		return websocket.Message.Send(p.conn, string(b))
+	} else {
+		return websocket.Message.Send(p.conn, b)
+	}
 }
 
 func (p *wsAdapter) Close() {
 	p.conn.Close()
 }
 
+//create binary ws handler
 func CreateWsHandler(auth Auth, hub *Hub) http.Handler {
+	return CreateWsHandlerEx(auth, hub, false)
+}
+
+//create ws handler, txt parameter is binary/text frame flag
+func CreateWsHandlerEx(auth Auth, hub *Hub, txt bool) http.Handler {
 	return websocket.Handler(func(ws *websocket.Conn) {
-		adapter := &wsAdapter{conn: ws}
+		adapter := &wsAdapter{conn: ws, txt: txt}
 		r := ws.Request()
 		uid, err := auth(r)
 		if err != nil {
