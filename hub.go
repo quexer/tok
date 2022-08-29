ce4cd66e566425dd8dcc7432a677c3ecea360a49
quexer <quexer@gmail.com>
2022-08-29 16:56:02 +0800

    feat: update Queue interface

--- hub.go
+++ hub.go
@@ -1,6 +1,7 @@
 package tok
 
 import (
+	"context"
 	"expvar"
 	"log"
 )
@@ -15,14 +16,14 @@ var (
 
 type checkFrame struct {
 	uid    interface{}
-	chBool chan bool //channel to return online status
+	chBool chan bool // channel to return online status
 }
 
 type downFrame struct {
 	uid   interface{}
 	ttl   uint32
 	data  []byte
-	chErr chan error //channel to read send result from
+	chErr chan error // channel to read send result from
 }
 
 type upFrame struct {
@@ -30,19 +31,19 @@ type upFrame struct {
 	data []byte
 }
 
-//HubConfig config struct for creating new Hub
+// HubConfig config struct for creating new Hub
 type HubConfig struct {
-	Actor Actor //Actor implement dispatch logic
-	Q     Queue //Message Queue, if nil, message to offline user will not be cached
-	Sso   bool  //If it's true, new connection  with same uid will kick off old ones
+	Actor Actor // Actor implement dispatch logic
+	Q     Queue // Message Queue, if nil, message to offline user will not be cached
+	Sso   bool  // If it's true, new connection  with same uid will kick off old ones
 }
 
-//Hub core of tok, dispatch message between connections
+// Hub core of tok, dispatch message between connections
 type Hub struct {
 	sso           bool
 	actor         Actor
 	q             Queue
-	cons          map[interface{}][]*connection //connection list
+	cons          map[interface{}][]*connection // connection list
 	chUp          chan *upFrame
 	chDown        chan *downFrame
 	chConState    chan *conState
@@ -107,10 +108,10 @@ func (p *Hub) run() {
 			}()
 		case ff := <-p.chDown:
 			if l := p.cons[ff.uid]; len(l) > 0 {
-				//online
+				// online
 				go p.down(ff, l)
 			} else {
-				//offline
+				// offline
 				if ff.ttl == 0 {
 					ff.chErr <- ErrOffline
 					close(ff.chErr)
@@ -123,7 +124,7 @@ func (p *Hub) run() {
 			cf.chBool <- ok
 			close(cf.chBool)
 		case uid := <-p.chReadSignal:
-			//only pop msg for online user
+			// only pop msg for online user
 			if len(p.cons[uid]) > 0 {
 				go p.popMsg(uid)
 			}
@@ -144,20 +145,20 @@ func (p *Hub) popMsg(uid interface{}) {
 	if p.q == nil {
 		return
 	}
-
+	ctx := context.TODO()
 	for {
-		b, err := p.q.Deq(uid)
+		b, err := p.q.Deq(ctx, uid)
 		if err != nil {
 			log.Println("deq error", err)
 			return
 		}
 		if len(b) == 0 {
-			//no more data in queue
+			// no more data in queue
 			return
 		}
 		expDeq.Add(1)
 		if err := p.Send(uid, b, 0); err != nil {
-			if err := p.q.Enq(uid, b); err != nil {
+			if err := p.q.Enq(ctx, uid, b); err != nil {
 				log.Println("re-cache err", err, uid)
 			}
 			return
@@ -165,32 +166,32 @@ func (p *Hub) popMsg(uid interface{}) {
 	}
 }
 
-//Send message to someone.
-//ttl is expiry seconds. 0 means only send to online user
-//If ttl = 0 and user is offline, ErrOffline will be returned.
-//If ttl > 0 and user is offline or online but send fail, message will be cached for ttl seconds.
+// Send message to someone.
+// ttl is expiry seconds. 0 means only send to online user
+// If ttl = 0 and user is offline, ErrOffline will be returned.
+// If ttl > 0 and user is offline or online but send fail, message will be cached for ttl seconds.
 func (p *Hub) Send(to interface{}, b []byte, ttl uint32) error {
 
 	ff := &downFrame{uid: to, data: b, ttl: ttl, chErr: make(chan error)}
 	p.chDown <- ff
 	err := <-ff.chErr
 	if ttl > 0 && err != nil {
-		//online send err
-		ff.chErr = make(chan error) //create new channel
+		// online send err
+		ff.chErr = make(chan error) // create new channel
 		go p.cache(ff)
 		return <-ff.chErr
 	}
 	return err
 }
 
-//CheckOnline return whether user online or not
+// CheckOnline return whether user online or not
 func (p *Hub) CheckOnline(uid interface{}) bool {
 	cf := &checkFrame{uid: uid, chBool: make(chan bool)}
 	p.chCheck <- cf
 	return <-cf.chBool
 }
 
-//Online query online user list
+// Online query online user list
 func (p *Hub) Online() []interface{} {
 	ch := make(chan []interface{})
 	p.chQueryOnline <- ch
@@ -199,13 +200,14 @@ func (p *Hub) Online() []interface{} {
 
 func (p *Hub) cache(ff *downFrame) {
 	defer close(ff.chErr)
+	ctx := context.TODO()
 	expEnq.Add(1)
 	if p.q == nil {
 		ff.chErr <- ErrQueueRequired
 		return
 	}
 
-	if err := p.q.Enq(ff.uid, ff.data, ff.ttl); err != nil {
+	if err := p.q.Enq(ctx, ff.uid, ff.data, ff.ttl); err != nil {
 		ff.chErr <- err
 	}
 }
@@ -236,7 +238,7 @@ func (p *Hub) goOffline(conn *connection) {
 	l := p.cons[conn.uid()]
 	rest := connExclude(l, conn)
 
-	//this connection has gotten offline, ignore
+	// this connection has gotten offline, ignore
 	if len(l) == len(rest) {
 		return
 	}
@@ -292,28 +294,28 @@ func (p *Hub) goOnline(conn *connection) {
 	if p.sso {
 		for _, c := range l {
 			if conn.ShareConn(c) {
-				continue //never close share connection
+				continue // never close share connection
 			}
-			//notify before close connection
+			// notify before close connection
 			go p.byeThenClose(conn.dv, c)
 		}
 		p.cons[conn.uid()] = []*connection{conn}
 		return
 	}
 
-	//it's a new connection
+	// it's a new connection
 	if len(connExclude(l, conn)) == len(l) {
 		l = append(l, conn)
 		p.cons[conn.uid()] = l
 	}
 }
 
-//tryDeliver try to deliver all messages, if uid is online
+// tryDeliver try to deliver all messages, if uid is online
 func (p *Hub) tryDeliver(uid interface{}) {
 	p.chReadSignal <- uid
 }
 
-//Kick kick all connections of uid
+// Kick kick all connections of uid
 func (p *Hub) Kick(uid interface{}) {
 	p.chKick <- uid
 }
@@ -322,7 +324,7 @@ func (p *Hub) stateChange(conn *connection, online bool) {
 	p.chConState <- &conState{conn, online}
 }
 
-//receive data from user
+// receive data from user
 func (p *Hub) receive(dv *Device, b []byte) {
 	p.chUp <- &upFrame{dv: dv, data: b}
 }
--- q.go
+++ q.go
@@ -1,8 +1,12 @@
 package tok
 
-//Queue is FIFO queue interface, used by Hub
+import (
+	"context"
+)
+
+// Queue is FIFO queue interface, used by Hub
 type Queue interface {
-	Enq(uid interface{}, data []byte, ttl ...uint32) error
-	Deq(uid interface{}) ([]byte, error)
-	Len(uid interface{}) (int, error)
+	Enq(ctx context.Context, uid interface{}, data []byte, ttl ...uint32) error
+	Deq(ctx context.Context, uid interface{}) ([]byte, error)
+	Len(ctx context.Context, uid interface{}) (int, error)
 }
